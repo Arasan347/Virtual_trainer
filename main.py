a37606e85425ecb0e21fa0a103838bc3fde7fa71
Arasan <arasansmart770@gmail.com>
2023-05-03 21:14:07 +0530

    Minimum code for pose detection with mediapipe is done user interface for trainer and changes in the bmi calculator

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import cv2
 import mediapipe as mp
 import time
-import numpy
+import numpy as np
 pTime = 0
 
 mp_draw = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
+
+# Calculating angles for bicep
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
 
 # with opencv capturing video
 cap = cv2.VideoCapture(0)
