e37e3eba7ef9034d2136b47aafa528d5579c8eda
Arasan <arasansmart770@gmail.com>
2023-05-04 16:27:45 +0530

    extracting landmarks

--- main.py
+++ main.py
@@ -8,19 +8,6 @@ mp_draw = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 
-# Calculating angles for bicep
-def calculate_angle(a, b, c):
-    a = np.array(a)
-    b = np.array(b)
-    c = np.array(c)
-
-    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
-    angle = np.abs(radians * 180.0 / np.pi)
-
-    if angle > 180.0:
-        angle = 360 - angle
-
-    return angle
 
 
 # with opencv capturing video
